f0c46015ace1c3e34811f53310921aa4687f3c55
Kenneth Assogba <kennethassogba@gmail.com>
2026-02-01 01:43:03 +0100

    Add NvChad config files

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+o.expandtab = true -- expand tab input with spaces characters
+o.smartindent = true -- syntax aware indentations for newline inserts
+o.shiftwidth = 4 -- spaces per indentation level
+o.tabstop = 8 -- num of space characters per tab
